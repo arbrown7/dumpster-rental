ed4b43a63299c180024412ddfd1cf5babdef5532
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-25 19:47:20 -0700

    rental model

--- src/models/rental/rental.js
+++ src/models/rental/rental.js
@@ -7,6 +7,18 @@ import {
     serverTimestamp
 } from "firebase/firestore";
 
+/**
+ * Core function that creates a rental based on user input.
+ * 
+ * @param {number} size - Size of dumpster
+ * @param {string} organization - HOA or organization associated with rental (if applicable)
+ * @param {string} address - Address of rental
+ * @param {string} placement - Placement instructions for dumpster
+ * @param {date} deliveryDate - Delivery date
+ * @param {date} pickupDate - Pickup date
+ * @param {bool} agreement - Customer's agreement to the requirements of the contract
+ * @returns {?} - I don't know if this needs to return anything...
+ */
 const createRental = async (
         size,
 		organization,
@@ -34,6 +46,41 @@ const createRental = async (
     //TODO: figure out what to return here
 };
 
+
+/**
+ * Finds all current rentals, meaning the current date is 
+ * between their delivery and pickup dates AND the rental 
+ * has been marked as paid.
+ * 
+ * @returns {id, name, phone, address, size, placement, deliveryDate, pickupDate, receiptNo, creationDate}
+ */
+const findCurrent = async () => {
+    let currentDate = Date.now();
+    //TODO: figure out how dates will be stored in the database
+
+    //const q = query(collection(db, "rental"), where())
+};
+
+/**
+ * Finds all past rentals, meaning their pickup date has passed.
+ * 
+ * @returns {?}
+ */
+const findHistory = async () => {
+
+};
+
+/**
+ * Updates the status of a rental
+ * 
+ * @param {number} rentalId - rental ID
+ * @param {string} status - new status
+ * @returns {?}
+ */
+const updateStatus = async () => {
+
+};
+
 const findByUserId = async (userId) => {
     const q = query(collection(db, "rental"), where("userId", "==", userId));
 
@@ -46,18 +93,6 @@ const findById = async (rentalId) => {
 
     const snapshot = await getDocs(q);
     return snapshot;
-};
-
-const findCurrent = async () => {
-
-};
-
-const findHistory = async () => {
-
-};
-
-const updateStatus = async () => {
-
 };
 
 export {
